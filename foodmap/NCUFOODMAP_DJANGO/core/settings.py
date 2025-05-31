34091fbd30967775860834ccd9e1f37ee81b58c5
Apoohow <hollmersi9488@gmail.com>
2025-06-01 02:09:17 +0800

    修正靜態檔案路徑設定

--- foodmap/NCUFOODMAP_DJANGO/core/settings.py
+++ foodmap/NCUFOODMAP_DJANGO/core/settings.py
@@ -142,8 +142,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATIC_ROOT = '/opt/render/project/src/foodmap/NCUFOODMAP_DJANGO/staticfiles'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
