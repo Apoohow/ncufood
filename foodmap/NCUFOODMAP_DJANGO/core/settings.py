9d4424ef1d3f9bb39073c0305b5b21fd09f11074
Apoohow <hollmersi9488@gmail.com>
2025-06-01 02:12:04 +0800

    修正靜態檔案路徑和構建命令

--- foodmap/NCUFOODMAP_DJANGO/core/settings.py
+++ foodmap/NCUFOODMAP_DJANGO/core/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/opt/render/project/src/foodmap/NCUFOODMAP_DJANGO/staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR.parent.parent, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
